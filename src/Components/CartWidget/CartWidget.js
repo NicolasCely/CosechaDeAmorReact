7b705272b3e055865ad3618be7826ba21b1a3348
Nicolas Cely <nikocely24@gmail.com>
2022-11-26 13:37:03 -0500

    navbar y lading

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+
+const CartWidget = () => {
+    return (
+        <button>
+            <img src={'./images/Carrito.png'} alt='Cart-Wigget'  width="60px" height="50px"/>  0   
+        </button>
+    )
+}
+
+export default CartWidget
